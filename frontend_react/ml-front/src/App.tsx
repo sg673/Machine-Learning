c7a81563ea4cebc42a9c9326204c6282940be77f
sg673 <sg673@student.le.ac.uk>
2025-09-05 13:36:59 +0100

    feat: added tailwind to stack

--- frontend_react/ml-front/src/App.tsx
+++ frontend_react/ml-front/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card underline font-bold">
         <button onClick={() => getRequest()}>
           count is {data}
         </button>
