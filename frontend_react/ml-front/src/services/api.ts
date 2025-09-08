e7f5e4009bbc907360a5e28cc3268baeaf6c0dca
sg673 <sg673@student.le.ac.uk>
2025-09-08 12:08:54 +0100

    feat: added crud operations to api

--- frontend_react/ml-front/src/services/api.ts
+++ frontend_react/ml-front/src/services/api.ts
@@ -2,6 +2,23 @@ const BASE_URL = "http://localhost:8080/api/v1/";
 
 export const api = {
     get: async (endpoint: string) => await fetch(`${BASE_URL}${endpoint}`),
+    post: async (endpoint: string, body: never) => await fetch(`${BASE_URL}${endpoint}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }),
+    put: async (endpoint: string, body: never) => await fetch(`${BASE_URL}${endpoint}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }),
+    delete: async (endpoint: string) => await fetch(`${BASE_URL}${endpoint}`, {
+        method: "DELETE"
+    }),
     
     test: async () => (await api.get("test")).text(),
 }
