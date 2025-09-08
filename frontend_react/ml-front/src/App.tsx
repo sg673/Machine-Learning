71dfcc428d3a46db46c30cdc29bc374aa697ab70
sg673 <sg673@student.le.ac.uk>
2025-09-08 09:54:29 +0100

    feat: added header

--- frontend_react/ml-front/src/App.tsx
+++ frontend_react/ml-front/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { api } from './services/api';
+import { Header } from './common/header';
 
 function App() {
   const [data, setData] = useState<string>("");
@@ -9,22 +10,25 @@ function App() {
     setData(await api.test());
   }
   return (
-    <div className='card' >
-      <button 
-        className='btn ' 
-        onClick={getRequest}>
-          <p>Response is: <span className=''>{data}</span></p>
-      </button>
-      <button 
-        className='btn-sec' 
-        onClick={getRequest}>
-          <p>Response is: <span className=''>{data}</span></p>
-      </button>
-      <div className='card'>
-        <div className='bg-acc p-0.5 rounded-lg m-2'></div>
+    <>
+      <Header></Header>
+      <div className='card' >
+        <button 
+          className='btn ' 
+          onClick={getRequest}>
+            <p>Response is: <span className=''>{data}</span></p>
+        </button>
+        <button 
+          className='btn-sec' 
+          onClick={getRequest}>
+            <p>Response is: <span className=''>{data}</span></p>
+        </button>
+        <div className='card' >
+          <div className='bg-acc p-0.5 rounded-lg m-2'></div>
 
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
