c5d2b966f0e80979c2d9e7b72ef0f6523eeb718a
sg673 <sg673@student.le.ac.uk>
2025-09-29 14:54:06 +0100

    refactor: changed website to use new api interface

--- frontend_react/ml-front/src/services/api.ts
+++ frontend_react/ml-front/src/services/api.ts
@@ -2,44 +2,6 @@ import type { Model, Training, Result } from "./constants";
 
 const BASE_URL = "http://localhost:8080/api/v1/";
 
-export const api = {
-  get: async (endpoint: string) => await fetch(`${BASE_URL}${endpoint}`),
-  post: async (endpoint: string, body: unknown) => await fetch(`${BASE_URL}${endpoint}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(body)
-  }),
-  put: async (endpoint: string, body: unknown) => await fetch(`${BASE_URL}${endpoint}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(body)
-  }),
-  delete: async (endpoint: string) => await fetch(`${BASE_URL}${endpoint}`, {
-    method: "DELETE"
-  }),
-
-  test: async () => (await api.get("test")).text(),
-
-  getDashboardStats: async () => (await api.get("dashboard/stats")).json(),
-
-  getModels: async () => (await api.get("models")).json(),
-  deleteModelById: async (id: string) => await api.delete(`models/${id}`),
-  getModelById: async (id: string) => (await api.get(`models/${id}`)).json(),
-  createModel: async (values: Model) => (await api.post("models", values)).json(),
-
-  startTraining: async (values: Model) => (await api.post("training/start", values)).json(),
-  getTrainingStatus: async (id: string) => (await api.get(`training/${id}/status`)).json(),
-  stopTraining: async (id: string) => await api.post(`training/${id}/stop`, {}),
-
-
-  // getResults: async () => (await api.get("results")).json(),
-  // getResultById: async (id: string) => (await api.get(`results/${id}`)).json(),
-}
-
 const request = async (endpoint: string, options: RequestInit = {}, timeout = 10000) => {
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
