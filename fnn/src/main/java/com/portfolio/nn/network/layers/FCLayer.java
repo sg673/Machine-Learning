4878fb5ab8f5c4997db364e4181713beb784bfae
sg673 <sg673@student.le.ac.uk>
2025-11-27 12:37:03 +0000

    refactor: update FCLayer constructor and forward method to handle 3D input

--- fnn/src/main/java/com/portfolio/nn/network/layers/FCLayer.java
+++ fnn/src/main/java/com/portfolio/nn/network/layers/FCLayer.java
@@ -6,12 +6,16 @@ public class FCLayer extends LayerBase {
   private double[][] weights;
   private int inputSize;
 
-  public FCLayer(int inputSize, int outputSize, ActivationFunction activationFunction) {
+  public FCLayer(int inputWidth, int inputHeight, int inputDepth, int outputSize, 
+                 ActivationFunction activationFunction) {
     super();
-    this.inputSize = inputSize;
+    this.inputWidth = inputWidth;
+    this.inputHeight = inputHeight;
+    this.inputDepth = inputDepth;
+    this.inputSize = inputWidth * inputHeight * inputDepth;
     this.size = outputSize;
     this.activFunc = activationFunction;
-
+    
     this.weights = new double[outputSize][inputSize];
     this.biases = new double[outputSize];
     for (int i = 0; i < outputSize; i++) {
@@ -22,17 +26,32 @@ public class FCLayer extends LayerBase {
     }
   }
 
-  public double[] forward(double[] input) {
+  public double[] forward(double[][][] input) {
+    // Inputs need to be flattened so they're not 3d
+    double[] flatInput = flatten(input);
     double[] output = new double[size];
-
+    
     for (int i = 0; i < size; i++) {
       double sum = biases[i];
       for (int j = 0; j < inputSize; j++) {
-        sum += weights[i][j] * input[j];
+        sum += weights[i][j] * flatInput[j];
       }
       output[i] = activFunc.activate(sum);
     }
     return output;
+  }
+
+  private double[] flatten(double[][][] input) {
+    double[] flat = new double[inputSize];
+    int index = 0;
+    for (int c = 0; c < inputDepth; c++) {
+      for (int y = 0; y < inputHeight; y++) {
+        for (int x = 0; x < inputWidth; x++) {
+          flat[index++] = input[c][y][x];
+        }
+      }
+    }
+    return flat;
   }
 
 }
