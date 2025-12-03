9db8f11809c7fee40e465f1950f6125dbc4344c2
sg673 <sg673@student.le.ac.uk>
2025-12-03 11:26:45 +0000

    feat: implement CNN demo with layer configuration and training logic

--- fnn/src/main/java/com/portfolio/nn/NeuralNetworkApplication.java
+++ fnn/src/main/java/com/portfolio/nn/NeuralNetworkApplication.java
@@ -7,17 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.portfolio.nn.data.DataLoader;
 import com.portfolio.nn.data.MNISTLoader;
+import com.portfolio.nn.network.ConvolutionalNetwork;
 import com.portfolio.nn.network.FeedForwardNetwork;
 import com.portfolio.nn.network.activation.ActivationFunction;
+import com.portfolio.nn.network.layers.ConvolutionalLayer;
+import com.portfolio.nn.network.layers.FCLayer;
+import com.portfolio.nn.network.layers.PoolingLayer;
+import com.portfolio.nn.network.layers.PoolingLayer.PoolingType;
 import com.portfolio.nn.util.DataUtils;
 
 @SuppressWarnings("unused")
 @SpringBootApplication
 public class NeuralNetworkApplication {
   public static void main(String[] args) {
-    SpringApplication.run(NeuralNetworkApplication.class, args);
+    //SpringApplication.run(NeuralNetworkApplication.class, args);
 
     // runMNISTDemo();
+    runCNNDemo();
   }
 
   private static void runMNISTDemo() {
@@ -38,6 +44,56 @@ public class NeuralNetworkApplication {
       double accuracy = network.evaluate(testData.getImages(), testLabels);
       System.out.println("Test Accuracy: " + (accuracy * 100) + "%");
       // network.save("t1");
+
+    } catch (IOException e) {
+      System.err.println("Error loading data: " + e);
+    }
+  }
+
+  private static void runCNNDemo() {
+    System.out.println("Starting Neural Network Framework - CNN Demo");
+
+    try {
+      DataLoader loader = new MNISTLoader();
+      DataLoader.Dataset trainData = loader.loadTraining();
+      DataLoader.Dataset testData = loader.loadTest();
+
+      ConvolutionalNetwork cnn = new ConvolutionalNetwork();
+
+      //28x28x1 -> 24x24x32 -> 12x12x32 -> 8x8x64 -> 4x4x64 -> 128 -> 10
+      // cnn.addLayer(
+      //   new ConvolutionalLayer(28,28,1,32,5,1,0,ActivationFunction.RELU)
+      // ).addLayer(
+      //   new PoolingLayer(24,24,32,2,2,PoolingLayer.PoolingType.MAX)
+      // ).addLayer(
+      //   new ConvolutionalLayer(12,12,32,64,5,1,0,ActivationFunction.RELU)
+      // ).addLayer(
+      //   new PoolingLayer(8,8,64,2,2,PoolingLayer.PoolingType.MAX)
+      // ).addLayer(
+      //   new FCLayer(4,4,64,128, ActivationFunction.RELU)
+      // ).addLayer(
+      //   new FCLayer(1,1,128,10,ActivationFunction.SIGMOID)
+      // );
+
+      cnn.addLayer(
+        new ConvolutionalLayer(28,28,1,32,3,1,0,ActivationFunction.RELU)
+      ).addLayer(
+        new ConvolutionalLayer(28,28,1,32,3,1,0,ActivationFunction.RELU)
+      ).addLayer(
+        new PoolingLayer(28, 28, 1, 2, 1, PoolingType.MAX)
+      ).addLayer(
+        new ConvolutionalLayer(14,14,1,64,3,1,0,ActivationFunction.RELU)
+      ).addLayer(
+        new PoolingLayer(14, 14, 1, 2, 1, PoolingType.MAX)
+      ).addLayer(
+        new FCLayer(7, 7, 1, 128, ActivationFunction.RELU)
+      ).addLayer(
+        new FCLayer(128, 1, 1, 10, ActivationFunction.SIGMOID)
+      );
+      
+      cnn.train(trainData.getImages(),DataUtils.oneHotEncode(trainData.getLabels()),0.001,1);
+      double accuracy = cnn.evaluate(testData.getImages(), DataUtils.oneHotEncode(testData.getLabels()));
+      System.out.println("Accuracy: " + (accuracy * 100) + "%");
 
     } catch (IOException e) {
       System.err.println("Error loading data: " + e);
