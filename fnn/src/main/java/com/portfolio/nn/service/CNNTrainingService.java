4ffded6be5a60cf29a82714e484a3958e9e2dd38
sg673 <sg673@student.le.ac.uk>
2026-01-13 23:52:08 +0000

    feat: implement CNNTrainingService to manage training sessions and handle concurrent training

---
+++ fnn/src/main/java/com/portfolio/nn/service/CNNTrainingService.java
@@ -0,0 +1,39 @@
+package com.portfolio.nn.service;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+
+import com.portfolio.nn.constants.DataSet;
+import com.portfolio.nn.model.CNNModel;
+import com.portfolio.nn.model.CNNTrainingParameters;
+import com.portfolio.nn.model.CNNTrainingSession;
+import com.portfolio.nn.network.ConvolutionalNetwork;
+
+
+@Service
+public class CNNTrainingService {
+
+  private final Map<String, CNNTrainingSession> sessions = new ConcurrentHashMap<>();
+
+  public String startTraining(CNNModel model, CNNTrainingParameters params){
+    String sessionId = UUID.randomUUID().toString();
+    ConvolutionalNetwork network = new ConvolutionalNetwork(DataSet.fromString(model.trainingData));
+    CNNTrainingSession session = new CNNTrainingSession(network, params, sessionId, sessionId);
+    sessions.put(sessionId, session);
+    
+    new Thread(() ->{
+      /**
+       * Init Layers in model 
+       * load dataset
+       * start training
+       * save on completion
+       */
+    });
+
+
+    return sessionId;
+  }
+}
