a12c157b039c242517d591b2c5f975767ca24624
sg673 <sg673@student.le.ac.uk>
2026-01-13 12:50:02 +0000

    feat: add CNN model service and controller with CRUD operations

---
+++ fnn/src/main/java/com/portfolio/nn/service/CNNModelService.java
@@ -0,0 +1,38 @@
+package com.portfolio.nn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.portfolio.nn.model.CNNModel;
+import com.portfolio.nn.repo.CNNRepo;
+
+@Service
+public class CNNModelService {
+  @Autowired
+  private CNNRepo repo;
+
+  public List<CNNModel> getAll(){
+    return repo.findAll();
+  }
+
+  public Optional<CNNModel> getModelById(String id){
+    return repo.findById(id);
+  }
+
+  public CNNModel saveModel(CNNModel model){
+    return repo.save(model);
+  }
+
+  public boolean deleteById(String id){
+    Optional<CNNModel> model = repo.findById(id);
+    if(model.isPresent()){
+      repo.deleteById(id);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
